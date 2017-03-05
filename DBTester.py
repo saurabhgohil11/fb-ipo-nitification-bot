db7e56a26e36cc56df16c4140b7f9dc66b6af754
Saurabh Gohil <saurabhgohil11@gmail.com>
2017-03-05 21:23:25 +0530

    tester class

--- DBTester.py
+++ DBTester.py
@@ -1,6 +1,25 @@
 import DBHelper
 import os
+import IPOHelper
+import app
+
+
+def log(message):  # simple wrapper for logging to stdout on heroku
+    print str(message)
+    sys.stdout.flush()
 
 DBHelper.dropTableIPO()
 DBHelper.dropTableUser()
+if not(os.path.isfile('ipocache.db')):
+    log("DB not exist crawling data and creating DB")
+    IPOCrawler.refreshData()
+    log("DONE: DB not exist crawling data and creating DB")
 
+log("----------Testing Today's IPO-------------")
+log(formResponse("today"))
+log("----------Testing Music IPO-------------")
+log(formResponse("IPO MUSIC"))
+log("----------Testing Current IPO-------------")
+log(formResponse("current"))
+log("----------Testing 10 IPO-------------")
+log(formResponse("list"))
