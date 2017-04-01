f72b0d252df56f3be6e09525faf27226d6bc18e5
Saurabh Gohil <saurabhgohil11@gmail.com>
2017-04-01 12:39:57 +0530

    Fixed Log issue

--- MyLogger.py
+++ MyLogger.py
@@ -5,5 +5,5 @@ from datetime import datetime
 def log(message):  # simple wrapper for MyLogger.logging to stdout on heroku
     print str(message)
     with open("logs.txt", "a") as myfile:
-        myfile.write(str(datetime.now())+" : "+message)
+        myfile.write(str(datetime.now())+" : "+str(message))
     sys.stdout.flush()
