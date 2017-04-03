c30ceaa2938cea6f45a536e29019ef4412eb6f81
Saurabh Gohil <saurabhgohil11@gmail.com>
2017-04-03 22:54:17 +0530

    pref try 2

--- MyLogger.py
+++ MyLogger.py
@@ -4,6 +4,6 @@ from datetime import datetime
 
 def log(message):  # simple wrapper for MyLogger.logging to stdout on heroku
     print str(message)
-    with open("logs.txt", "a") as myfile:
-        myfile.write(str(datetime.now())+" : "+str(message))
+    # with open("logs.txt", "a") as myfile:
+        # myfile.write(str(datetime.now())+" : "+str(message))
     sys.stdout.flush()
