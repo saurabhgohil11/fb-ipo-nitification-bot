e73723080ba708a5b0e7bd4db88218d20b0ce344
Saurabh Gohil <saurabhgohil11@gmail.com>
2017-05-07 22:16:43 +0530

    fix bug when user sends non text msg

--- app.py
+++ app.py
@@ -73,6 +73,8 @@ def webhook():
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                    if not (messaging_event["message"]).get("text"):
+                        continue
                     message_text = messaging_event["message"]["text"]  # the message's text
                     DBHelper.insertUser(sender_id)
                     responseList = formResponse(sender_id,message_text)
