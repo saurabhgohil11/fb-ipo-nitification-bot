c9e0cfc01402e7f61555a0bcc52c515d5fefcb6d
Saurabh Gohil <saurabhgohil11@gmail.com>
2017-05-09 22:59:28 +0530

    New Log system

--- MyLogger.py
+++ MyLogger.py
@@ -1,5 +1,11 @@
 import sys
-from datetime import datetime
+from logentries import LogentriesHandler
+import logging
+
+log = logging.getLogger('logentries')
+log.setLevel(logging.INFO)
+
+log.addHandler(LogentriesHandler('06380d85-3900-4507-913b-0b9d4f06f3b7'))
 
 
 def log(message):  # simple wrapper for MyLogger.logging to stdout on heroku
@@ -7,3 +13,4 @@ def log(message):  # simple wrapper for MyLogger.logging to stdout on heroku
     # with open("logs.txt", "a") as myfile:
         # myfile.write(str(datetime.now())+" : "+str(message))
     sys.stdout.flush()
+    log.info(message)
